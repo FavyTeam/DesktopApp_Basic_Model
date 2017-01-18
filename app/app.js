744dabd14f0f99e1bc741974f91c9bf1251e228c
FavyTeam <a.popov1030@gmail.com>
2017-01-18 12:23:43 +0200

    version 1.0.1
    First View added in version 1..0.1

---
+++ app/app.js
@@ -0,0 +1,15 @@
+import { Component } from 'angular2/core';
+import { bootstrap } from 'angular2/platform/browser';
+
+@Component({
+    selector: 'myapp',
+    template: '<h1>We build new version of desktop application</h1>'
+})
+
+export class AppComponent {
+  constructor() {
+
+  }
+}
+
+bootstrap(AppComponent);
